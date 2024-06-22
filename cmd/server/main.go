5514c0cf8d4a178d025e7f925299d85f013d890e
Joel Gomes <joel.gomes@farfetch.com>
2024-06-22 14:55:25 +0100

    feat: updated main.go to run service

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"scoreplay/internal/controller"
+	"scoreplay/internal/mapper"
+	"scoreplay/internal/repository"
+	"scoreplay/internal/router"
+	"scoreplay/internal/service"
+	"scoreplay/internal/service/thirdparty"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// Initialize repositories
+	teamRepo := repository.NewInMemoryTeamRepository()
+	playerRepo := repository.NewInMemoryPlayerRepository()
+
+	// Initialize mappers
+	teamMapper := mapper.NewDefaultTeamMapper()
+	playerMapper := mapper.NewDefaultPlayerMapper()
+
+	// Initialize services
+	theSportsDBService := thirdparty.NewDefaultTheSportsBDService()
+	teamService := service.NewDefaultTeamService(teamRepo, playerRepo, teamMapper, playerMapper, theSportsDBService)
+	playerService := service.NewDefaultPlayerService(playerRepo, playerMapper)
+
+	// Initialize validator
+	validator := validator.New()
+
+	// Initialize controllers
+	teamController := controller.NewTeamController(teamService, validator)
+	playerController := controller.NewPlayerController(playerService, validator)
+
+	// Initialize controller struct
+	controller := &controller.Controller{
+		Team:   teamController,
+		Player: playerController,
+	}
+
+	// Initialize Gin router
+	router := gin.Default()
+
+	// Setup routes
+	routers.SetupTeamRoutes(router, controller.Team)
+	routers.SetupPlayerRoutes(router, controller.Player)
+
+	// Run the server
+	router.Run(":5555")
 }
